923f615315f31ce4a7ea481e10353f89106e8368
Roman <nimda.xd@gmail.com>
2020-01-25 15:40:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+print("Привет!!! Узнай свой знак зодиака.")
+q=input("Введите месяц (слово): ")
+w=int(input("Введите число (день рождения): "))
+if q=="март" and w>=21 and w<=31:
+ print("Овен")
+elif q=="апрель" and w>=1 and w<=20:
+ print("Овен")
+elif q=="апрель" and w>=21 and w<=31:
+ print("Телец")
+elif q=="май" and w>=1 and w <=20:
+ print("Телец")
+elif q=="май" and w>=21 and w<=31:
+ print("Близнецы")
+elif q=="июнь" and w>=1 and w<=21:
+ print("Близнецы")
+elif q=="июнь" and w>=22 and w<=31:
+ print("PAK")
+elif q=="июль" and w>=1 and w<=22:
+ print("PAK")
+elif q=="июль" and w>=23 and w<=31:
+ print("Лев")
+elif q=="авгут" and w>=1 and w<=23:
+ print("Лев")
+elif q=="авгут" and w>=24 and w<=31:
+ print("Дева")
+elif q=="сентябрь" and w>=1 and w<=23:
+ print("Дева")
+elif q=="сентябрь" and w>=24 and w<=31:
+ print("Весы")
+elif q=="октябрь" and w>=1 and w<=23:
+ print("Весы")
+elif q=="октябрь" and w>=24 and w<=31:
+ print("Скорпион")
+elif q=="ноябрь" and w>=1 and w<=22:
+ print("Скорпион")
+elif q=="ноябрь" and w>=23 and w<=31:
+ print("Стрелец")
+elif q=="декабрь" and w>=1 and w<=21:
+ print("Стрелец")
+elif q=="декабрь" and w>=22 and w<=31:
+ print("Козерог")
+elif q=="Январь" and w>=1 and w<=20:
+ print("Козерог")
+elif q=="Январь" and w>=21 and w<=31:
+ print("Водолей")
+elif q=="февраль" and w>=1 and w<=20:
+ print("Водолей")
+elif q=="февраль" and w>=21 and w<=31:
+ print("Рыбы")
+elif q==3 and w>=1 and w<=20:
+ print("Рыбы")
